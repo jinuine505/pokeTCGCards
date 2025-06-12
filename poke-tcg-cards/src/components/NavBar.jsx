3922eae03ac026cb67d7715925b3e21cd0c0908e
jinuine505 <nguyenj0708@gmail.com>
2025-06-12 03:35:17 -0400

    Add the navigation bar + unfinished favorites page

---
+++ poke-tcg-cards/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router'
+import '../css/NavBar.css'
+import logo from '/pokeball.webp'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-brand">
+                <Link to="/"><img src={logo} title="Pokemon TCG Database" /></Link>
+            </div>
+            <div className="nav-links">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/favorites">Favorites</Link>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar
