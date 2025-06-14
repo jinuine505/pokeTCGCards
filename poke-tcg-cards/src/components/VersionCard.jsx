5c987e65dee48258ba46506502b74bdf45c11126
jinuine505 <nguyenj0708@gmail.com>
2025-06-14 05:32:04 -0400

    The version list now has an image of the card for every card version

--- poke-tcg-cards/src/components/VersionCard.jsx
+++ poke-tcg-cards/src/components/VersionCard.jsx
@@ -5,6 +5,7 @@ const VersionCard = ({ set, setId, cardNum, card }) => {
     return (
         <div className="version-card">
             <Link className="card-content" to={`/${setId}/${cardNum}`}>
+                <img src={`${card.image}`} alt={card.id} title={card.name} />
                 <p className="card-name">{card.name}</p>
                 <p className="set-name">{set.name}</p>
                 <p className="card-rarity">{card.rarity}</p>
