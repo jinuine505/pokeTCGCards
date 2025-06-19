5566e96890e176454d9e73231420a2438c509905
jinuine505 <nguyenj0708@gmail.com>
2025-06-19 01:19:00 -0400

    Redo collected cards push
    - Collected cards are saved into local storage
    - The check within the button will be green once the card is collected
    - The card image is grayed out until it has been marked as collected

--- poke-tcg-cards/src/components/CardCard.jsx
+++ poke-tcg-cards/src/components/CardCard.jsx
@@ -3,13 +3,17 @@ import { Link } from "react-router";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useStarredContext } from "../context/StarredContext";
+import { useCollectedContext } from "../context/CollectedContext";
+
 
 const CardCard = ({ card, setId }) => {
     const { toggleStarred, isStarred } = useStarredContext();
+    const { toggleCollected, isCollected } = useCollectedContext();
     const cardNum = card.id.split("-")[1];
 
-    const handleComplete = (e) => {
+    const handleCollect = (e) => {
         e.preventDefault();
+        toggleCollected(card);
     }
 
       const handleStar = (e) => {
@@ -21,10 +25,10 @@ const CardCard = ({ card, setId }) => {
         <div className="card-card">
             <div className="card-content">
                 <Link to={`/${setId}/${cardNum}`} className="card-image">
-                    <img src={`${card.image}`} alt={card.id} title={card.name} />
+                    <img className={isCollected(card) ? "collected" : ""} src={`${card.image}`} alt={card.id} title={card.name} />
                 </Link>
                 <div className="card-btns">
-                    <button className="collect-btn" title="Check" onClick={handleComplete}>
+                    <button className={`collect-btn ${isCollected(card) ? "collected" : ""}`} title="Check" onClick={handleCollect}>
                         <FontAwesomeIcon icon={faCheck} />
                     </button>
                     <button className={`star-btn ${isStarred(card) ? "starred" : ""}`} title="Star" onClick={handleStar}>
--- poke-tcg-cards/src/main.jsx
+++ poke-tcg-cards/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StarredProvider } from './context/StarredContext.jsx';
+import { CollectedProvider } from './context/CollectedContext.jsx';
 
 const queryClient = new QueryClient();
 
@@ -12,9 +13,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <StarredProvider>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
+        <CollectedProvider>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </CollectedProvider>
       </StarredProvider>
     </BrowserRouter>
   </StrictMode>,
