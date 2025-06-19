0de2dd70a2fec3521a06b21096bd9fde92537157
jinuine505 <nguyenj0708@gmail.com>
2025-06-19 01:24:02 -0400

    Card image in version list now dims to reflect collected state

--- poke-tcg-cards/src/components/VersionCard.jsx
+++ poke-tcg-cards/src/components/VersionCard.jsx
@@ -1,11 +1,14 @@
 import "../css/VersionCard.css"
+import { useCollectedContext } from "../context/CollectedContext";
 import { Link } from "react-router";
 
 const VersionCard = ({ set, setId, cardNum, card }) => {
+    const { isCollected } = useCollectedContext();
+
     return (
         <div className="version-card">
             <Link className="card-content" to={`/${setId}/${cardNum}`}>
-                <img src={`${card.image}`} alt={card.id} title={card.name} />
+                <img className={isCollected(card) ? "collected" : ""} src={`${card.image}`} alt={card.id} title={card.name} />
                 <p className="card-name">{card.name}</p>
                 <p className="set-name">{set.name}</p>
                 <p className="card-rarity">{card.rarity}</p>
