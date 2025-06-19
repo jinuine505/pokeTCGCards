930b9e9997211d71d836bb82718c54aa679a3600
jinuine505 <nguyenj0708@gmail.com>
2025-06-19 17:43:23 -0400

    Small tweak for page routing

--- poke-tcg-cards/src/main.jsx
+++ poke-tcg-cards/src/main.jsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pokemon-tcg-pocket-tracker">
       <StarredProvider>
         <CollectedProvider>
           <QueryClientProvider client={queryClient}>
